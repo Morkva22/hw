db388a22c7c6de80d05a048c17330abbadc6eb61
lexa <alekseiholovati@gmai.com>
2024-11-20 19:49:04 +0200

    task2

--- 152.cpp
+++ 152.cpp
@@ -5,5 +5,20 @@
 using namespace std;
 
 int main() {
+	int x, y, result = 1;
 
+	cout << "write number x:";
+	cout << "write number y:";
+	cin >> x;
+	cin >> y;
+
+	if (y < 0) {
+		cout << "cant be minus number";
+		return 1;
+	}
+	for (int i = 1; i <= y; ++i) {
+		result *= x;
+	}
+	cout << x << " y stepeni " << y << " dorivnue " << result << endl;
+	return 0;
 }
